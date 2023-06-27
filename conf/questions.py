d68d839f0dfd5c2117fb55869bddcde7a0dce9ef
chakra1166 <chakravarthy.ch@tigeranalytics.com>
2023-06-27 14:45:46 +0530

    Update questions.py

--- conf/questions.py
+++ conf/questions.py
@@ -3,6 +3,6 @@ sample_q = [
     "What is the revenue for the year 2023",
     "What are the total liabilities and total borrowings for the year 2023",
     "List the key events where MoU was signed",
-    "List strategic alliances of yonsin",
-    "What does yonsin have to do with bcsd malaysia",
+    "List strategic alliances of Yinson",
+    "What does Yinson have to do with bcsd Malaysia",
 ]
